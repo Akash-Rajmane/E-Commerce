891cbb96b630011acb30c809e8d94bc608ff50f6
Akash <akashrajmane23@gmail.com>
2023-11-18 19:25:26 +0530

    added home page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -3,12 +3,30 @@ import React from 'react'
 const About = () => {
   return (
   <>
+    <div className='bg-secondary p-4' style={{marginTop:"-30px"}}> 
+      <h1 className='text-center text-white font-weight-bold' style={{fontSize:"95px"}}>The Generics</h1>
+    </div>
     <h1 className='text-center'>About Us</h1>
     <div className='mt-4 p-4'>
         <img src={"https://prasadyash2411.github.io/ecom-website/img/Band%20Members.png"} alt={"band"} style={{width:"200px", height:"200px", borderRadius:"50%"}} className='float-start m-4'/>
         <p className='p-4'>
         Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of sorrows, hates no prosecutors will unfold in the enduring of which were born in it? Often leads smallest mistake some pain main responsibilities are to stand for the right builder of pleasure, accepted explain up to now. , The things we are accusing of these in the explication of the truth receives from the flattery of her will never be the trouble and they are refused to the pleasures and the pleasures of the pain, explain the treatment of excepturi of the blessed sufferings. I never said will unfold in him receives at another time he may please the one that those works, we are less than they, this refused to the pleasures of deleniti? Those are! Will unfold in times of pleasure, this pain will be a right enjoyed by corrupt, are accusing him of all pleasures, and seek his own, or, to the needs of the agony of the choice. We hate the fellow. Lorem ipsum dolor, sit amet consectetur rebates. The distinction, that arise from or to. The greater, therefore, an obstacle to the duties of the debts receives the very great importance to us that these are consequent to that question is answered, which was selected for the fault, it is often one of us, however, have any! Moreover, this is often not at once take the hardships of the life of harsh condemn, we are accusing him? Him whom something large cisterns.
         </p>
+    </div>
+    <div className='bg-info p-4'>
+      <h1 className='text-start text-white font-weight-bold' style={{marginLeft:"200px"}}>The Generics</h1>
+      <div className='float-end d-flex gap-4' style={{marginTop:"-50px", marginRight:"100px"}}>
+        <img src={"https://prasadyash2411.github.io/ecom-website/img/6260efc8fc9a9002669d2f4ad9956cc0.jpg"} 
+        alt="youtube" 
+        style={{width:"40px",height:"40px"}}/>
+        <img src={"https://prasadyash2411.github.io/ecom-website/img/Spotify%20Logo.png"}
+         alt="spotify"
+         style={{width:"40px",height:"40px"}}/>
+        <img src={"https://prasadyash2411.github.io/ecom-website/img/Facebook%20Logo.png"} 
+        alt="fb"
+        style={{width:"40px",height:"40px"}}
+        />
+      </div>
     </div>
   </>
   )
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,10 +3,54 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-    <h1 className='text-center'>Welcome To E-Commmerce</h1>
-    <p className='p-4'>
-        Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of sorrows, hates no prosecutors will unfold in the enduring of which were born in it? Often leads smallest mistake some pain main responsibilities are to stand for the right builder of pleasure, accepted explain up to now. , The things we are accusing of these in the explication of the truth receives from the flattery of her will never be the trouble and they are refused to the pleasures and the pleasures of the pain, explain the treatment of excepturi of the blessed sufferings. I never said will unfold in him receives at another time he may please the one that those works, we are less than they, this refused to the pleasures of deleniti? Those are! Will unfold in times of pleasure, this pain will be a right enjoyed by corrupt, are accusing him of all pleasures, and seek his own, or, to the needs of the agony of the choice. We hate the fellow. Lorem ipsum dolor, sit amet consectetur rebates. The distinction, that arise from or to. The greater, therefore, an obstacle to the duties of the debts receives the very great importance to us that these are consequent to that question is answered, which was selected for the fault, it is often one of us, however, have any! Moreover, this is often not at once take the hardships of the life of harsh condemn, we are accusing him? Him whom something large cisterns.
-    </p>
+        <div className='bg-secondary p-4' style={{marginTop:"-30px"}}> 
+            <h1 className='text-center text-white font-weight-bold' style={{fontSize:"95px"}}>The Generics</h1>
+        </div>
+        <h1 className='text-center'>Welcome To E-Commmerce</h1>
+        <div className='mt-3'>
+          <h3 className='text-center font-weight-bold'>TOURS</h3>
+          <ul className='list-unstyled'>
+            <li className='d-flex justify-content-center align-items-center gap-4 p-2' style={{borderBottom: "2px solid #000", width:"600px", margin: "auto"}}>
+              <span>JUL 16</span>
+              <span className='text-secondary'>DETROIT, MI</span>
+              <span className='text-secondary'>DTE ENERGY MUSIC THEATRE</span>
+              <button className='btn btn-info text-white'>BUY TICKETS</button>
+            </li>
+            <li className='d-flex justify-content-center align-items-center gap-4 p-2' style={{borderBottom: "2px solid #000", width:"600px", margin: "auto"}}>
+              <span>JUL 19</span>
+              <span className='text-secondary'>DETROIT, MI</span>
+              <span className='text-secondary'>DTE ENERGY MUSIC THEATRE</span>
+              <button className='btn btn-info text-white'>BUY TICKETS</button>
+            </li>
+            <li className='d-flex justify-content-center align-items-center gap-4 p-2' style={{borderBottom: "2px solid #000", width:"600px", margin: "auto"}}>
+              <span>JUL 22</span>
+              <span className='text-secondary'>DETROIT, MI</span>
+              <span className='text-secondary'>DTE ENERGY MUSIC THEATRE</span>
+              <button className='btn btn-info text-white'>BUY TICKETS</button>
+            </li>
+            <li className='d-flex justify-content-center align-items-center gap-4 p-2' style={{borderBottom: "2px solid #000", width:"600px", margin: "auto"}}>
+              <span>JUL 29</span>
+              <span className='text-secondary'>DETROIT, MI</span>
+              <span className='text-secondary'>DTE ENERGY MUSIC THEATRE</span>
+              <button className='btn btn-info text-white'>BUY TICKETS</button>
+            </li>
+            <li className='d-flex justify-content-center align-items-center gap-4 p-2' style={{borderBottom: "2px solid #000", width:"600px", margin: "auto"}}>
+              <span>AUG 2</span>
+              <span className='text-secondary'>DETROIT, MI</span>
+              <span className='text-secondary'>DTE ENERGY MUSIC THEATRE</span>
+              <button className='btn btn-info text-white'>BUY TICKETS</button>
+            </li>
+            <li className='d-flex justify-content-center align-items-center gap-4 p-2' style={{borderBottom: "2px solid #000", width:"600px", margin: "auto"}}>
+              <span>AUG 7</span>
+              <span className='text-secondary'>DETROIT, MI</span>
+              <span className='text-secondary'>DTE ENERGY MUSIC THEATRE</span>
+              <button className='btn btn-info text-white'>BUY TICKETS</button>
+            </li>
+          </ul>
+        </div>
+        <div className='bg-info p-4 position-relative bottom-0 start-0 w-100'>
+            <h1 className='text-start text-white font-weight-bold ' style={{marginLeft:"200px"}}>The Generics</h1>
+        </div>
  
     </>
   )
