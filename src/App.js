0e2bf9e262e47106008b423db43c923ae4f61f43
Akash <akashrajmane23@gmail.com>
2023-11-18 18:30:14 +0530

    added routing and pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
 import Header from './components/header/Header';
-import Products from './components/products/Products';
+import Products from './pages/products/Products';
 import Cart from './components/cart/Cart';
 import { CartContextProvider } from './context/cart-context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
 
 
 function App() {
   const [isCartShown, setIsCartShown] = useState(false);
   return (
+    <Router>
     <CartContextProvider>
       <Header setIsCartShown={setIsCartShown}/>
+      {isCartShown && <Cart setIsCartShown={setIsCartShown}/>} 
       <main style={{marginTop:"80px"}}>
-      <Products/>
-      {isCartShown && <Cart setIsCartShown={setIsCartShown}/>}
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/store" element={<Products/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
       </main>
       
     </CartContextProvider>
+    </Router>
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,23 @@
 import React, { useContext } from 'react';
 import CartContext from '../../context/cart-context';
+import { NavLink } from 'react-router-dom';
 
 const Header = ({setIsCartShown}) => {
   const {items} = useContext(CartContext);
+  
 
   const totalCartItems = items.reduce((total,curr)=>{
     return total + Number(curr.quantity);
   },0);
 
+
   return (
     <header className="position-fixed w-100 top-0 start-0 bg-dark text-white d-flex justify-content-around align-items-center gap-4" style={{height:"60px", zIndex:"10"}}>
         <nav className='d-flex justify-content-center align-items-center'>
             <ul className="d-flex align-items-center list-unstyled gap-4 mt-3 fs-5">
-                <li className='mx-4'>Home</li>
-                <li className='mx-4'>Store</li>
-                <li className='mx-4'>About</li>
+                <li className='mx-4'><NavLink to="/" className="text-decoration-none" style={({isActive}) => isActive ? {color:"dodgerblue"} : {color:"#fff"}}>Home</NavLink></li>
+                <li className='mx-4'><NavLink to="/store" className="text-decoration-none"  style={({isActive}) => isActive ? {color:"dodgerblue"} : {color:"#fff"}}>Store</NavLink></li>
+                <li className='mx-4'><NavLink to="about" className="text-decoration-none"  style={({isActive}) => isActive ? {color:"dodgerblue"} : {color:"#fff"}}>About Us</NavLink></li>
             </ul>
         </nav>
         <button className='btn btn-primary d-flex gap-2' onClick={()=>setIsCartShown(flag=>!flag)}><span>Cart</span><span>{totalCartItems}</span></button>
